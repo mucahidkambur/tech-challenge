ba3c039b400321085c257087af047caa41232012
Mücahit KAMBUR <mucahit-kambur@hotmail.com>
2019-09-19 22:08:18 +0300

    Add login views and conditions

--- app/src/main/java/com/mucahitkambur/kariyerchallenge/ui/login/LoginFragment.kt
+++ app/src/main/java/com/mucahitkambur/kariyerchallenge/ui/login/LoginFragment.kt
@@ -6,12 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.mucahitkambur.kariyerchallenge.R
 import com.mucahitkambur.kariyerchallenge.databinding.FragmentLoginBinding
 import com.mucahitkambur.kariyerchallenge.di.Injectable
 import com.mucahitkambur.kariyerchallenge.ui.MainViewModel
+import com.mucahitkambur.kariyerchallenge.util.USER_NAME
+import com.mucahitkambur.kariyerchallenge.util.USER_PASSWORD
 import com.mucahitkambur.kariyerchallenge.util.viewModelProvider
 import javax.inject.Inject
 
@@ -37,7 +40,27 @@ class LoginFragment : Fragment(), Injectable {
         ).apply {
             lifecycleOwner = this@LoginFragment
         }
-
         return dataBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        initListener()
+    }
+
+    private fun initListener() {
+
+        // Giriş buton listener'ı
+        dataBinding.buttonLogin.setOnClickListener {
+            val username = dataBinding.textPerson.text.toString()
+            val password = dataBinding.textPassword.text.toString()
+
+            if (username.isEmpty() || password.isEmpty()){
+                Toast.makeText(context, resources.getString(R.string.login_error), Toast.LENGTH_SHORT).show()
+            }else if (username == USER_NAME && password == USER_PASSWORD){
+                Toast.makeText(context, "Girdi", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/mucahitkambur/kariyerchallenge/util/Constant.kt
+++ app/src/main/java/com/mucahitkambur/kariyerchallenge/util/Constant.kt
@@ -3,3 +3,5 @@ package com.mucahitkambur.kariyerchallenge.util
 const val API_URL = "https://kariyertechchallenge.free.beeceptor.com"
 const val NO_CONNECTION = "Lütfen internet bağlantınızı kontrol ediniz."
 const val ERROR_MESSAGE = "unknown"
+const val USER_NAME = "test"
+const val USER_PASSWORD = "test"
